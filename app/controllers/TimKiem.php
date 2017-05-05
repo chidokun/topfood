2ba7c48db5059b4bb839a9a5a3f3f64e0d4b055b
Nguyễn Tuấn <nguyentuanit96@gmail.com>
2017-05-05 20:30:57 +0700

    Commit dangNhap

---
+++ app/controllers/TimKiem.php
@@ -0,0 +1,9 @@
+<?php
+class TimKiem extends CI_Controller {
+	public function index() {
+		//$data['main_content'] = 'dangKy';
+		//$this->load->view('layouts/main-register',$data);
+		$this->load->view('timKiem');
+	}
+}
+?>
